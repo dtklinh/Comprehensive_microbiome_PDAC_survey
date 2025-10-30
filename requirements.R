1e3f757793e27d65064355b357ba09d1d4fc7dc6
dtklinh <dangtruongkhanhlinh@gmail.com>
2025-10-30 16:04:59 +0100

    add requirement

---
+++ requirements.R
@@ -0,0 +1,22 @@
+# if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+# BiocManager::install(c("phyloseq", "microbiome", "ComplexHeatmap"), update = FALSE)
+# 
+# install.packages(
+#   "microViz",
+#   repos = c(davidbarnett = "https://david-barnett.r-universe.dev", getOption("repos"))
+# )
+
+# requirements.R
+required_pkgs <- c(
+  "tidyverse",
+  "vegan",
+  "DESeq2",
+  "Wrench",
+  "ggtext",
+  "ggraph",
+  "DT",
+  "corncob"
+)
+
+to_install <- setdiff(required_pkgs, rownames(installed.packages()))
+if (length(to_install)) BiocManager::install(to_install)
