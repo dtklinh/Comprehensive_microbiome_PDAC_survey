0eb067009f87e9a937a831e45cf4050e5838ea3e
dtklinh <dangtruongkhanhlinh@gmail.com>
2025-11-02 15:59:36 +0100

    update main in Chap3

---
+++ Rscripts/Chap3/main.R
@@ -0,0 +1,10 @@
+### Chapter 3 in manusctipt, mouse sample including FF and FFPE
+library(phyloseq)
+library(microbiome)
+library(microViz)
+library(vegan)
+library(Wrench)
+library(tidyverse)
+library(dplyr)
+
+pseq_org <- readRDS("./data/Chap3/pseq_Proj5_postFilter_v04.rds")
