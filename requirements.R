ea0f7b1705bda158141918a2617e0c16e9b1dc15
dtklinh <dangtruongkhanhlinh@gmail.com>
2025-11-03 12:05:17 +0100

    update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,10 +1,12 @@
-# if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-# BiocManager::install(c("phyloseq", "microbiome", "ComplexHeatmap"), update = FALSE)
-# 
-# install.packages(
-#   "microViz",
-#   repos = c(davidbarnett = "https://david-barnett.r-universe.dev", getOption("repos"))
-# )
+## if nesscessary, install libglpk-dev
+## sudo apt install libcairo2-dev libfontconfig1-dev pandoc
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+BiocManager::install(c("phyloseq", "microbiome", "ComplexHeatmap"), update = FALSE)
+
+install.packages(
+  "microViz",
+  repos = c(davidbarnett = "https://david-barnett.r-universe.dev", getOption("repos"))
+)
 
 # requirements.R
 required_pkgs <- c(
