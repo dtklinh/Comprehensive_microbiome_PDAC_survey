65cfdb6e7d14b87c3c74d58ed4c616207fac564b
dtklinh <dangtruongkhanhlinh@gmail.com>
2025-11-03 13:43:35 +0100

    add to main.R

--- Rscripts/Chap3/main.R
+++ Rscripts/Chap3/main.R
@@ -7,4 +7,14 @@ library(Wrench)
 library(tidyverse)
 library(dplyr)
 
-pseq_org <- readRDS("./data/Chap3/pseq_Proj5_postFilter_v04.rds")
+pseq_org <-         readRDS("./data/Chap3/pseq_Proj5_postFilter_v04.rds")
+pseq_decontam <-    readRDS("./data/Chap3/pseq_bulk_decontam_p0.5.rds")
+pseq_Fisher <-      readRDS("./data/Chap3/pseq_bulk_Fisher.rds")
+pseq_restrictive <- readRDS("./data/Chap3/pseq_bulk_restrictive.rds")
+pseq_SCRuB <-       readRDS("./data/Chap3/pseq_bulk_SCRuB.rds")
+
+## investigate most prevalenace species in each methods
+
+pseq_SCRuB %>% prevalence() %>% 
+  tibble(taxa = names(.), value = .) %>% 
+  filter(value >=0.5)
