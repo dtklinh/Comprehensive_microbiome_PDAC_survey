1805a411f4cb02260207e1ceae592b961411dc03
dtklinh <dangtruongkhanhlinh@gmail.com>
2026-08-30 14:11:37 +0200

    add for punlication

--- Rscripts/ForPublication/tmp_process.R
+++ Rscripts/ForPublication/tmp_process.R
@@ -9,6 +9,16 @@ library(tidyverse)
 
 pseq_nct <- readRDS("./Rscripts/ForPublication/data/longitudinal_contaminant_survey/NCT_2026April_v0.rds")
 
+## Chap1: get metadata
+df_meta <- meta(pseq)
+df_meta <- df_meta %>% 
+  rownames_to_column(var = "ID") %>% 
+  dplyr::select(ID, Run, Barcode, LP = Person, Control_type, Sequencing_date = Seq_date) 
+
+library(openxlsx)
+write.xlsx(df_meta, "Chap1.xlsx")
+  
+
 #dirname(rstudioapi::getSourceEditorContext()$path) %>% print()
 df_meta <- meta(pseq_nct)
 
@@ -17,9 +27,19 @@ ps <- rprojroot::find_rstudio_root_file() %>%
   file.path("data/Chap3", "pseq_Proj5_postFilter_v04.rds") %>% 
   readRDS()
 df_meta <- meta(ps)
+df_meta_FF_R1 <- df_meta %>% 
+  dplyr::filter(ffpe.bulk == "bulk") %>% 
+  rownames_to_column(var = "ID")
+write.xlsx(df_meta_FF_R1, "KPC_FF_R1.xlsx")
+df_meta_FFPE <- df_meta %>% 
+  dplyr::filter(ffpe.bulk == "FFPE") %>% 
+  rownames_to_column(var = "ID")
+write.xlsx(df_meta_FFPE, "KPC_FFPE.xlsx")
 
 ## FF replica
 ff_replica <- rprojroot::find_rstudio_root_file() %>%
   file.path("data/Chap3_Addition/pseq_origin_v1.0.rds") %>% 
   readRDS()
-meta_ff_rep <- meta(ff_replica)
+meta_ff_rep <- meta(ff_replica) %>% 
+  rownames_to_column(var = "ID")
+write.xlsx(meta_ff_rep, "KPC_FF_R2.xlsx")
